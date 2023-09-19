d3a1728f5d08ab1e87342a9285433172a3e043ad
Axel-Negron <axel.negron2@upr.edu>
2023-09-19 12:34:10 -0400

    Styling,added config file, added shortcuts, more

--- Just-Type.py
+++ Just-Type.py
@@ -3,18 +3,39 @@ from tkinter.filedialog import askopenfilename, asksaveasfilename
 import tkinter.font as tk_font
 from tkinter import ttk
 from tkinter.colorchooser import askcolor
+import configparser
 
 
+
+
+############################Init####################################
 window = Tk()
 data = StringVar()
 filepath = StringVar()
 window.title("Just Type")
 
 
+def load_config(config:configparser.ConfigParser):
 
+    cur_font_name.set(config['Text']['font_name'])
+    cur_font_size.set(config['Text']['font_size'])
+    cur_font_color.set(config['Text']['font_color'])
 
+    cur_bg1.set(config['Display']['bg_color1'])
+    cur_bg2.set(config["Display"]['bg_color2'])
+    cur_bg3.set(config["Display"]['bg_color3'])
+    return
 
-#Font variables#
+def ret_available_fonts():
+    fonts = tk_font.families()
+
+    return fonts
+
+fonts = ret_available_fonts()
+size_values = [1, 2, 4, 6, 8, 10, 12, 14, 16, 18,
+               20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
+
+#################Font variables#################
 
 filepath.set('')
 cur_tags = IntVar()
@@ -22,21 +43,15 @@ cur_tags = IntVar()
 cur_tags.set(0)
 cur_font_name = StringVar()
 cur_font_size = IntVar()
-cur_font_name.set('Calibri')
-cur_font_size.set(14)
 cur_font_color = StringVar()
-cur_font_color.set('#000000')
 
-#Window colors
+########################### Window colors
 
 cur_bg1 = StringVar()
-cur_bg1.set("#edffd6")
 cur_bg2 = StringVar()
-cur_bg2.set("#edffd6")
 cur_bg3 = StringVar()
-cur_bg3.set("#edffd6")
 
-############################
+##############################################
 cur_txt_addon = StringVar()
 cur_txt_addon.set('normal')
 
@@ -53,19 +68,24 @@ is_underline = BooleanVar()
 alive_btn = []
 alive_widgets = []
 
+rewrite_config = BooleanVar()
+rewrite_config.set(0)
+config_file = 'config.ini'
+config = configparser.ConfigParser()
+config.read(config_file)
+load_config(config)
 
-def ret_available_fonts():
-    fonts = tk_font.families()
+i=IntVar()
+combo_set = 0
+for font in fonts:
+    if font=='Calibri':
+        combo_set= i.get()
+        break
+    
+    i.set(i.get()+1)
 
-    return fonts
+#########################:##################################################
 
-
-fonts = ret_available_fonts()
-
-cur_font_name.set(fonts[1])
-size_values = [1, 2, 4, 6, 8, 10, 12, 14, 16, 18,
-               20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
-cur_font_size.set(size_values[5])
 
 # MENU FUNCTIONS
 
@@ -142,19 +162,19 @@ def enter_update(event):
 def open_settings():
 
     sidebar_wdth = '10'
-    settings_wdw = Toplevel(window)
+    settings_wdw = Toplevel(window,background=cur_bg2.get())
     settings_wdw.geometry("590x500")
     settings_wdw.title("Settings")
-    sidebar = Frame(settings_wdw, width=sidebar_wdth)
+    sidebar = Frame(settings_wdw,background=cur_bg2.get(), width=sidebar_wdth)
     sidebar.pack(side=LEFT, fill='y',)
-    txt_settings_btn = Button(sidebar, text="Text", width=sidebar_wdth,
+    txt_settings_btn = Button(sidebar, text="Text",background=cur_bg3.get(),foreground=cur_font_color.get(), width=sidebar_wdth,
                               command=lambda: disp_txt_settings(settings_wdw, alive_btn), pady=10)
     txt_settings_btn.pack()
 
     if txt_settings_btn["state"] == DISABLED:
         txt_settings_btn["state"] = NORMAL
 
-    paper_settings_btn = Button(sidebar, text="Paper", width=sidebar_wdth,
+    paper_settings_btn = Button(sidebar, text="Paper",background=cur_bg3.get(),foreground=cur_font_color.get(), width=sidebar_wdth,
                                 command=lambda: disp_paper_settings(settings_wdw, alive_btn), pady=10)
     paper_settings_btn.pack()
 
@@ -164,9 +184,16 @@ def open_settings():
     alive_btn.append(txt_settings_btn)
     alive_btn.append(paper_settings_btn)
 
+    
+
     return
 
-###############################
+def rewrite():
+    if(rewrite_config.get()):
+        with open (config_file,'w') as file:
+            file.write(config_file)
+
+#########################################################
 
 
 # Menu and Text Widget
@@ -219,6 +246,7 @@ def txt_addons():
             is_bold.set(1)
         else:
             is_bold.set(0)
+            boldvar.set(0)
 
     if italicvar.get() or is_italic.get() != FALSE:
         if is_italic.get() != 1:
@@ -227,6 +255,7 @@ def txt_addons():
 
         else:
             is_italic.set(0)
+            boldvar.set(0)
 
     if underlinevar.get() or is_underline.get() != FALSE:
         if is_underline.get() != 1:
@@ -235,6 +264,7 @@ def txt_addons():
 
         else:
             is_underline.set(0)
+            boldvar.set(0)
 
     for addon in addons:
         font_txt += addon+''
@@ -312,6 +342,20 @@ def get_combobox_selected(combobox_font: ttk.Combobox, combobox_size: ttk.Combob
     return
 
 
+def color_updator():
+    txt.config(background=cur_bg1.get())
+    txt_menu.config(background=cur_bg2.get())
+    italic_check.config(background=cur_bg3.get())
+    underline_check.config(background=cur_bg3.get())
+    bold_check.config(background=cur_bg3.get())
+    apply_btn.config(background=cur_bg3.get())
+
+    config.set('Display','bg_color1',cur_bg1.get())
+    config.set('Display','bg_color2',cur_bg2.get())
+    config.set('Display','bg_color3',cur_bg3.get())
+    
+
+
 def change_font_color():
     try:
         color = askcolor()
@@ -321,6 +365,63 @@ def change_font_color():
     except:
         print("Log - Couldn't set color")
         
+    return
+
+
+def underline_shrt(event):
+    if underlinevar.get()==1:
+        underlinevar.set(0)
+        txt_addons()
+        return
+    update_txt(cur_font_name.get(),cur_font_size.get(),cur_tags.get())
+    underlinevar.set(1)
+    txt_addons()
+    
+
+
+def bold_shrt(event):
+    if boldvar.get()==1:
+        boldvar.set(0)
+        txt_addons()
+        return
+    update_txt(cur_font_name.get(),cur_font_size.get(),cur_tags.get())
+    boldvar.set(1)
+    txt_addons()
+    
+
+def italic_shrt(event):
+    if italicvar.get()==1:
+        italicvar.set(0)
+        txt_addons()
+        return
+    update_txt(cur_font_name.get(),cur_font_size.get(),cur_tags.get())
+    italicvar.set(1)
+    txt_addons()
+    
+
+def appearance_colors(selector:int):
+    match selector:
+
+        case 1:
+            try: 
+                cur_bg1.set(askcolor()[1])
+            except:
+                print('error picking color')
+
+        case 2:
+            try: 
+                cur_bg2.set(askcolor()[1])
+            except:
+                print('error picking color')
+
+        case 3:
+            try: 
+                cur_bg3.set(askcolor()[1])
+            except:
+                print('error picking color')
+
+    color_updator()
+
     return
 
 
@@ -338,18 +439,18 @@ def disp_txt_settings(wndw: Toplevel, alivebtn: list):
     except:
         print("paper settings not alive, ignoring state")
 
-    r_view = Frame(wndw)
+    r_view = Frame(wndw,background=cur_bg2.get())
     r_view.pack(side=LEFT, fill='both', padx=5)
 
-    rl_view = Frame(r_view)
+    rl_view = Frame(r_view,background=cur_bg2.get())
     rl_view.pack(side=LEFT, fill='y', padx=15)
 
-    rr_view = Frame(r_view)
+    rr_view = Frame(r_view,background=cur_bg2.get())
     rr_view.pack(side=LEFT, fill='y', padx=5)
 
-    font = Label(rl_view, text="Font: ", width=5, height=2)
+    font = Label(rl_view, text="Font: ", width=5, height=2,background=cur_bg3.get(),foreground=cur_font_color.get())
     font.pack()
-    font_boxes = Frame(rl_view)
+    font_boxes = Frame(rl_view,background=cur_bg2.get())
 
     font_name_cmbx = ttk.Combobox(
         font_boxes, values=ret_available_fonts(), width=15)
@@ -361,18 +462,19 @@ def disp_txt_settings(wndw: Toplevel, alivebtn: list):
     font_boxes.pack()
 
     font_cmbx_apply = Button(rr_view, command=lambda: get_combobox_selected(
-        font_name_cmbx, font_size_cmbx, cur_tags.get()), text="Apply", pady=15, width=5)
+        font_name_cmbx, font_size_cmbx, cur_tags.get()), text="Apply",background=cur_bg3.get(),foreground=cur_font_color.get(), pady=15, width=5)
+    
     font_cmbx_apply.pack()
 
-    color_picker_lbl = Label(rl_view, text="Change font color: ", pady=15)
+    color_picker_lbl = Label(rl_view, text="Change font color: ",background=cur_bg3.get(),foreground=cur_font_color.get(), pady=15)
     color_picker = Button(rl_view, text="Pick",
-                          command=change_font_color, pady=15, width=20)
+                          command=change_font_color,background=cur_bg3.get(),foreground=cur_font_color.get(), pady=15, width=20)
 
     color_picker_lbl.pack()
     color_picker.pack()
 
     view_color = Label(rr_view, text="ABCDE", width=5,
-                       fg=cur_font_color.get(), pady=25)
+                       fg=cur_font_color.get(),background=cur_bg3.get(),foreground=cur_font_color.get(), pady=25)
     view_color.pack()
 
     alive_widgets.append(r_view)
@@ -395,45 +497,73 @@ def disp_paper_settings(wndw: Toplevel, alivebtn: list):
     except:
         print("Font settings not alive")
 
-    # rr_paper = Frame(wndw)
-    # rr_paper.pack()
+    r_view = Frame(wndw,background=cur_bg2.get())
+    r_view.pack(side=LEFT, fill='both', padx=5)
+
+    rl_view = Frame(r_view,background=cur_bg2.get())
+    rl_view.pack(side=LEFT, padx=5,fill='y')
+
+    appearance_lbl = Label(rl_view,text='Appearance: ',background=cur_bg3.get(),foreground=cur_font_color.get())
+    appearance_lbl.pack()
+
+    colors_frame = Frame(rl_view,background=cur_bg2.get())
+    colors_frame.pack()
+
+    appearance_color1 = Label(colors_frame,text='Color 1',background=cur_bg3.get(),foreground=cur_font_color.get(),width=9)
+    appearance_color1.pack(side=LEFT)
+    appearance_color2 = Label(colors_frame,text='Color 2',background=cur_bg3.get(),foreground=cur_font_color.get(),width=9)
+    appearance_color2.pack(side=LEFT)
+    appearance_color3 = Label(colors_frame,text='Color 3',background=cur_bg3.get(),foreground=cur_font_color.get(),width=9)    
+    appearance_color3.pack(side=LEFT)
+
+    color_btn_frm = Frame(rl_view,background=cur_bg2.get())
+    color_btn_frm.pack()
     
+    color_picker1 = Button(color_btn_frm,text="Pick",command=lambda: appearance_colors(1),background=cur_bg3.get(),foreground=cur_font_color.get(),width=9)
+    color_picker2 = Button(color_btn_frm,text="Pick",command=lambda: appearance_colors(2),background=cur_bg3.get(),foreground=cur_font_color.get(),width=9)
+    color_picker3 = Button(color_btn_frm,text="Pick",command=lambda: appearance_colors(3),background=cur_bg3.get(),foreground=cur_font_color.get(),width=9)
+
+    color_picker1.pack(side=LEFT)
+    color_picker2.pack(side=LEFT)
+    color_picker3.pack(side=LEFT)
+
+    rewrite_system = Frame(rl_view,background=cur_bg2.get())
+    rewrite_system.pack()
+    store_system_settings = Checkbutton(rewrite_system,text='Save to system',background=cur_bg3.get(),foreground=cur_font_color.get(),onvalue=True,offvalue=False,variable=rewrite_config)
+    store_btn = Button(rewrite_system,text='Apply',background=cur_bg3.get(),foreground=cur_font_color.get(),width=5,command=rewrite)
+    store_system_settings.pack(side=LEFT)
+    store_btn.pack(side=LEFT)
+
+    rr_view = Frame(wndw,background=cur_bg2.get())
+    rr_view.pack(side=RIGHT)
+
 
     
-    # rl_paper = Frame(wndw)
-    # rl_paper.pack()
-
-
-
-
-
+    alive_widgets.append(r_view)
+    alive_widgets.append(rr_view)
+    alive_widgets.append(rl_view)
 
     return
 
 
-
-    
-
-
-# Buttons
+######################################################## Buttons
 font_cmb = ttk.Combobox(txt_menu, values=fonts, width=10)
-font_cmb.current(1)
+font_cmb.current(i.get())
 font_cmb.pack(side=LEFT)
 
 size_cmb = ttk.Combobox(txt_menu, values=size_values, width=5)
-size_cmb.current(5)
+size_cmb.current(4)
 size_cmb.pack(side=LEFT)
 
 bold_check = Checkbutton(txt_menu, text='Bold', font=(cur_font_name.get(
-), 12, 'bold'), onvalue=True, offvalue=False, variable=boldvar, command=txt_addons)
+), 12, 'bold'), foreground=cur_font_color.get(),background=cur_bg3.get(),onvalue=True, offvalue=False, variable=boldvar, command=txt_addons)
 italic_check = Checkbutton(txt_menu, text='Italic', font=(cur_font_name.get(
-), 12, 'italic'), onvalue=True, offvalue=False, variable=italicvar, command=txt_addons)
+), 12, 'italic'),foreground=cur_font_color.get(),background=cur_bg3.get() ,onvalue=True, offvalue=False, variable=italicvar, command=txt_addons)
 
 underline_check = Checkbutton(txt_menu, text='Underline', font=(cur_font_name.get(
-), 12, 'underline'), onvalue=True, offvalue=False, variable=underlinevar, command=txt_addons)
+), 12, 'underline'),foreground=cur_font_color.get() ,background=cur_bg3.get(),onvalue=True, offvalue=False, variable=underlinevar, command=txt_addons)
 
-
-apply_btn = Button(txt_menu, text="Update", width=5, height=1, command=lambda: update_txt(
+apply_btn = Button(txt_menu, foreground=cur_font_color.get(),background=cur_bg3.get(),text="Update", width=5, height=1, command=lambda: update_txt(
     font_cmb.get(), size_cmb.get(), cur_tags.get()))
 apply_btn.pack(side=LEFT)
 bold_check.pack(side=LEFT, padx=3)
@@ -456,15 +586,18 @@ filemenu.add_command(label='Save As...', command=save_text_with_tags)
 filemenu.add_command(label='Settings', command=open_settings)
 
 
-# Keybinds
+##################################################### Keybinds
 
 txt.bind("<Return>", enter_update)
 txt.bind("<space>", enter_update)
 txt.bind("<less>", decrease_font)
 txt.bind("<greater>", increase_font)
 txt.bind("<Control-s>",save_shrt)
+txt.bind("<Control-u>",underline_shrt)
+txt.bind("<Control-b>",bold_shrt)
+txt.bind("<Control-i>",italic_shrt)
 
-#################################
+#####################################################
 
 
 window.mainloop()
